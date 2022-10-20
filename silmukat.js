e5f5a3e19f9b93d0ffe442a69f244bf9650b934c
EkaterinaM85 <mangka22@rpkk.fi>
2022-10-20 12:17:12 +0300

    eka versio

---
+++ silmukat.js
@@ -0,0 +1,57 @@
+//tehtävä 1
+for(i=1; i<=100; i++){
+console.log(i);
+} 
+//tehtävä 2
+for(i=0; i<=100; i++){
+    if(i%2==0)
+    console.log(i);
+}
+//tehtävä 3
+for(i=1; i<100; i++){
+    if(i%2)
+     console.log(i);
+}
+//tehtävä 4
+for(i=100; i>0; i--){
+     console.log(i);
+}
+//tehtävä 5
+for(i=100; i>0; i=i-3){
+    console.log(i);
+}   
+//tehtävä 6
+for(i=0; i<=100; i++){
+    console.log("apina");
+    console.log("banani");
+}   
+//tehtävä 7
+let sum = 0;
+for (let i =0; i<=100; i++){
+if (i%2) sum +=i;
+}
+console.log(sum);
+
+//tehtävä 7 (toinen ratkaisu)
+let summ = 0;
+for (let i =0; i<100; i++){
+if (i%2 == 1) summ +=i;
+}
+console.log(summ);
+
+//tehtävä 8
+let su = 0;
+for (let i =0; i<=100; i++){
+ su +=i*i;
+}
+console.log(su);
+
+//tehtävä 9
+let s = 1;
+for (let i = 1; i<=100; i++){
+ s *= i;
+}
+ console.log(s);
+
+//tehtävä 10
+
